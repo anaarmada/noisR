8255d003a311052067dea3b3d0b1e87be4ca3b80
Ana Armada <ana.f.armada@tecnico.ulisboa.pt>
2024-01-14 10:22:23 +0000

    estatistica

--- estatística_1.py
+++ estatística_1.py
@@ -18,25 +18,6 @@ def convert_to_string(data):
 teste = list(read_txt_file("seq1.txt"))
 teste.reverse()
 
-"""def integers(lst,max):
-    current_integer=0
-    i=0
-    new=[]
-    while len(lst)>i:
-        if lst[i]=='1' and current_integer+2**i<=max:
-            current_integer=current_integer+2**i
-            i+=1
-        elif lst[i]=='0':
-            i+=1
-        elif current_integer+2**i>max:
-            new.append(current_integer)
-            for j in range(0,i):
-                lst.pop(0)
-            i=0
-            current_integer=0
-    new.append(current_integer)
-    return new"""
-
 def integers(lst,new):
     current=0
     if len(lst)>=6:
